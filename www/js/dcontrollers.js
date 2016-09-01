c7bb3fdf1c2167b6c689a8422342614642fa07c4
Anurag Misra <anuragmisra1994@gmail.com>
2016-09-01 18:29:06 +0530

    made changes

--- www/js/dcontrollers.js
+++ www/js/dcontrollers.js
@@ -1,33 +1,98 @@
-angular.module('starter.dcontrollers', ['ionic', 'ionic-material'])
+ angular.module('starter.dcontrollers', ['ionic', 'ionic-material'])
 
-
-
-.controller('dSymptifyCtrl', function($scope, $stateParams, $state){
+.controller('dSymptifyCtrl', function($scope, $stateParams, $state, ionicMaterialInk){
 
 	$scope.me="Jaishriram";
+    ionicMaterialInk.displayEffect();
 })
 
-.controller('dFeedCtrl', function($scope, $stateParams, $state){
+.controller('dFeedCtrl', function($scope, $stateParams, $ionicPopup, $timeout, $state, ionicMaterialInk){
 
 	$scope.me="Jaishriram";
+    ionicMaterialInk.displayEffect();
+
+// var myPopup = $ionicPopup.show({
+//      template: '<input type="password" ng-model="data.wifi">',
+//      title: 'Enter Wi-Fi Password',
+//      subTitle: 'Please use normal things',
+//      scope: $scope,
+//      buttons: [
+//        { text: 'Cancel' },
+//        {
+//          text: '<b>Save</b>',
+//          type: 'button-positive',
+//          onTap: function(e) {
+//            if (!$scope.data.wifi) {
+//              //don't allow the user to close unless he enters wifi password
+//              e.preventDefault();
+//            } else {
+//              return $scope.data.wifi;
+//            }
+//          }
+//        },
+//      ]
+//    });
+
+//    myPopup.then(function(res) {
+//      console.log('Tapped!', res);
+//    });
+
+
+    $scope.showPopup = function() {
+        var alertPopup = $ionicPopup.show({
+        	template: '<select> <option>Blue</option> <option selected>Green</option> <option>Red</option> </select> <select> <option>Only Connections</option> <option selected>Followers</option> </select>',
+            title: 'Share',
+            subTitle: 'Select one of your groups to share', 
+            scope: $scope, 
+            buttons: [
+            	{ text: 'Cancel' },
+            	{
+            		text: '<b>Share</b>', 
+            		type: 'button-positive',
+         			onTap: function(e) {
+           			if (!$scope.data.wifi) {
+             //don't allow the user to close unless he enters wifi password
+             			e.preventDefault();
+           			} else {
+             		return $scope.data.wifi;
+           			}
+         		}
+       		},
+     		]
+        });
+
+        $timeout(function() {
+            //ionic.material.ink.displayEffect();
+            ionicMaterialInk.displayEffect();
+        }, 0);
+    };
+
 })
 
-.controller('dGroupsCtrl', function($scope, $stateParams, $state){
+.controller('dGroupsCtrl', function($scope, $stateParams, $state, ionicMaterialInk){
 
 	$scope.me="Jaishriram";
+    ionicMaterialInk.displayEffect();
 })
 
-.controller('dNotificationsCtrl', function($scope, $stateParams, $state){
+.controller('dNotificationsCtrl', function($scope, $stateParams, $state, ionicMaterialInk){
 
 	$scope.me="Jaishriram";
+    ionicMaterialInk.displayEffect();
 })
 
 
+.controller('dCommentsCtrl', function($scope, $stateParams, $state, $timeout, ionicMaterialInk, ionicMaterialMotion){
+
+    ionicMaterialInk.displayEffect();
+	$scope.me="Jaishriram";
+	    // Set Header
+
+    // Set Motion
+    ionicMaterialMotion.fadeSlideInRight();
 
 
 
-.controller('dDashCtrl', function($scope, $stateParams, $state){
 
-	$scope.me="Fuck";
 })
 
