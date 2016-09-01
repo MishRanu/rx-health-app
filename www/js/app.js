c4f7d5f28a35b5e869a3e7f4bdf5fff019ff8913
sndpmhr <sandeep053@gmail.com>
2016-09-01 18:26:58 +0530

    groups

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic', 'ionic-material', 'starter.controllers', 'starter.dcontrollers']);
+var app = angular.module('starter', ['ionic', 'ionic-material' ,'RxHealth.services', 'starter.controllers', 'starter.dcontrollers']);
 
-app.run(function ($ionicPlatform) {
+app.run(function (Http,$ionicPlatform, $state, $ionicPopup, $ionicHistory, $ionicLoading) {
     $ionicPlatform.ready(function () {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
--- www/js/dcontrollers.js
+++ www/js/dcontrollers.js
@@ -12,9 +12,30 @@ angular.module('starter.dcontrollers', ['ionic', 'ionic-material'])
 	$scope.me="Jaishriram";
 })
 
-.controller('dGroupsCtrl', function($scope, $stateParams, $state){
+.controller('dGroupsCtrl', function($scope, $stateParams, $state,Http,$ionicLoading){
 
 	$scope.me="Jaishriram";
+	Http.post('getcommunities', {
+      'UserID': 4
+    })
+    .success(function(data) {
+      $scope.ResponseCode = data.Status.ResponseCode;
+      $scope.ResponseMessage = data.Status.ResponseMessage;
+      $ionicLoading.hide();
+      if ($scope.ResponseCode == 200) {
+        $scope.myCommunities = data.Status.myCommunities;
+        $scope.otherCommunities = data.Status.otherCommunities;
+        $scope.following = data.Status.following;
+        console.dir($scope.myCommunities,$scope.following);
+      } else {
+        alert($scope.ResponseMessage);
+      }
+    }).error(function(data, status, headers, config) {
+        //$scope.data.error={message: error, status: status};
+        alert("error" + data);
+        $ionicLoading.hide();
+      });
+
 })
 
 .controller('dNotificationsCtrl', function($scope, $stateParams, $state){
