531951e3d5063a7d885c42d13004ce34923cae4e
Anurag Misra <anuragmisra1994@gmail.com>
2016-09-08 15:10:03 +0530

    updated feeds: done tamo-abhishek work

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,6 +17,36 @@ angular.module('starter.controllers', ['ionic', 'ionic-material'])
 		  $scope.commentmodal = modal;
 		});
 
+
+    $scope.timeSince = function(date) {
+
+    date = Date.parse(date);
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+}
+
   $scope.$on('ngLastRepeat.mylist',function(e) {
   ionicMaterialInk.displayEffect();
 })
